2cc2a4f6af2bde09734e4b3756dc01e627b8fed2
Eike Holtz <github@eholtz.de>
2016-09-19 08:37:38 +0200

    more human readable time

--- control/functions.sh
+++ control/functions.sh
@@ -39,3 +39,18 @@ if [ $(gpio -g read $1) -ne $(getonv $1) ] ; then
 fi
 }
 
+function hrt() {
+  # human readable time
+  [ -z "$1" ] || [ $1 -lt 1 ] && echo "0 Sekunden" && return 0
+  sekunden=$1
+  tage=$(($sekunden/86400))
+  sekunden=$(($sekunden%86400))
+  stunden=$(($sekunden/3600))
+  sekunden=$(($sekunden%3600))
+  minuten=$(($sekunden/60))
+  sekunden=$(($sekunden%60))
+  [ $tage -gt 0 ] && echo "$tage Tage "
+  [ $(($stunden+$tage)) -gt 0 ] && echo "$stunden Stunden "
+  [ $(($minuten+$stunden+$tage)) -gt 0 ] && echo "$minuten Minuten "
+  echo "$sekunden Sekunden"
+}
