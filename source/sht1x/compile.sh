78468c600bc6dcc88fa62efdf51206c7cb1cc868
Eike Holtz <github@eholtz.de>
2017-04-15 20:44:51 +0000

    use rpi v2 and finish compile

--- source/sht1x/compile.sh
+++ source/sht1x/compile.sh
@@ -7,10 +7,10 @@ cp RPi_SHT1x.h RPi_SHT1x_$1.h
 cp read_sensor.c read_sensor_$1.c
 sed -i "s/##MYPIN##/$1/g" RPi_SHT1x_$1.c
 sed -i "s/##MYPIN##/$1/g" RPi_SHT1x_$1.h
-sed -i "S/##MYPIN##/$1/g" read_sensor_$1.c
+sed -i "s/##MYPIN##/$1/g" read_sensor_$1.c
 
 bcmsrc=$(find ../ -iname bcm2835.c | head -n 1)
-gcc -lm -o read_sensor_$i $bcmsrc ./RPi_SHT1x_s$i.c read_sensor_$1.c
+gcc -lm -o read_sensor_$1 $bcmsrc ./RPi_SHT1x_$1.c read_sensor_$1.c
 
 rm RPi_SHT1x_$1.c
 rm RPi_SHT1x_$1.h
