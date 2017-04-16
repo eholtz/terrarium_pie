c1b1ea73ad9b53e16c3c9b7bfec082c798577a46
Eike Holtz <github@eholtz.de>
2017-04-16 10:32:50 +0000

    make it work

--- script/last_sensor_data.sh
+++ script/last_sensor_data.sh
@@ -2,9 +2,9 @@
 
 declare -A temperature
 
-for i in "${sensors[@]}" ; do
-  sensorfile="${rrd_sensor}_$i"
+for i in "${!sensors[@]}" ; do
+  sensorfile="${rrd_sensor}_${sensors[$i]}"
   temperature[$i]=$(rrdtool lastupdate $sensorfile | tail -n 1 | awk '{print $2}')
-  echo "Temperature from sensor $i (${sensoridname[$i]}) is ${temperature[$i]}"
+  echo "Temperature from sensor ${sensors[$i]} (${sensoridname[$i]}) is ${temperature[$i]}"
 done
 
