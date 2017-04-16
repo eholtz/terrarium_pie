11e21abc3bc591708fcc2c5f8d54395440b24bf4
Eike Holtz <github@eholtz.de>
2017-04-16 13:45:30 +0200

    print precise output

--- script/last_sensor_data.sh
+++ script/last_sensor_data.sh
@@ -4,7 +4,8 @@ declare -A temperature
 
 for i in "${!sensors[@]}" ; do
   sensorfile="${rrd_sensor}_${sensors[$i]}"
-  temperature[$i]=$(rrdtool lastupdate $sensorfile | tail -n 1 | awk '{print $2}' | cut -d '.' -f 1)
-  echo "Temperature from sensor ${sensors[$i]} (${sensoridname[$i]}) is ${temperature[$i]}"
+  temp=$(rrdtool lastupdate $sensorfile | tail -n 1 | awk '{print $2}')
+  temperature[$i]=$(echo $temp | cut -d '.' -f 1)
+  echo "Temperature from sensor ${sensors[$i]} (${sensoridname[$i]}) is ${temperature[$i]} ($temp)"
 done
 
