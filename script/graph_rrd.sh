22fd045e02a1113cf111b4ba7b9264738945b08e
Eike Holtz <github@eholtz.de>
2017-04-17 10:21:08 +0000

    code cleanup and correct °

--- script/graph_rrd.sh
+++ script/graph_rrd.sh
@@ -32,12 +32,11 @@ for i in 1_12h 2_7d 3_4w 4_1y ; do
   for s in "${!sensoridname[@]}" ; do
     DEF="$DEF DEF:v${s}=${rrd_sensor}_${sensors[$s]}:temperature:AVERAGE"
     VDEF="$VDEF VDEF:v${s}l=v${s},LAST"
-    GRAPH="$GRAPH LINE1:v${s}#${tmpcol[$counter]}:\"${sensoridname[$s]}\t\" GPRINT:v${s}l:\"%2.1lf %%\l\""
+    GRAPH="$GRAPH LINE1:v${s}#${tmpcol[$counter]}:\"${sensoridname[$s]}\t\" GPRINT:v${s}l:\"%2.1lf °C\l\""
     counter=$(($counter+1))
   done
   echo "timeout 10 rrdtool graph $dir_html/${i}_temperature.png --lazy --end $lastvalue --start end-${span} --tabwidth 60 --width 1024 --title \"Temperatur - ${span}\" $DEF $VDEF $GRAPH" > $dir_volatile/rrd_execute
   bash $dir_volatile/rrd_execute
-  cat $dir_volatile/rrd_execute
 
   DEF=""
   VDEF=""
@@ -51,7 +50,6 @@ for i in 1_12h 2_7d 3_4w 4_1y ; do
   done
   echo "timeout 10 rrdtool graph $dir_html/${i}_humidity.png --lazy --end $lastvalue --start end-${span} --tabwidth 60 --width 1024 --title \"Luftfeuchtigkeit - ${span}\" $DEF $VDEF $GRAPH" > $dir_volatile/rrd_execute
   bash $dir_volatile/rrd_execute
-  cat $dir_volatile/rrd_execute
 
   for r in "${!relaispins[@]}" ; do
     DEF="DEF:v${r}=${rrd_gpio}:pin${relaispins[$r]}:AVERAGE"
@@ -59,7 +57,6 @@ for i in 1_12h 2_7d 3_4w 4_1y ; do
 #    GRAPH="AREA:v${r}#aaffaa:${relaispinname[$r]}:skipscale"
     echo "timeout 10 rrdtool graph $dir_html/${i}_z_gpio_${r}.png --lazy --end $lastvalue --start end-${span} --width 1024 --height 30 --lower-limit 0 --upper-limit 1 --rigid --y-grid 1:3 --title \"${relaispinname[$r]}\" $DEF $GRAPH" > $dir_volatile/rrd_execute
     bash $dir_volatile/rrd_execute
-    cat $dir_volatile/rrd_execute
   done
 done
 
