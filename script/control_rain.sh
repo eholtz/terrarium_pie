f20128540db92b7087b3057fa5474e0912f1dc8d
Eike Holtz <github@eholtz.de>
2017-04-22 06:37:58 +0000

    a couple of bugfixes

--- script/control_rain.sh
+++ script/control_rain.sh
@@ -6,12 +6,12 @@ spot_timeout=1600
 
 if [ $epoch_rain_start -gt 0 ]; then
   echo "Today it will rain ..."
-  if [ [ $(($epoch_rain_start-$spot_timeout)) -gt $epoch_current ] && [ $(($epoch_rain_stop+$spot_timeout)) -lt $epoch_current ] ]; then
-    echo "It will rain or has been raining within the next $spot_timeout seconds, to turn off the spots"
+  if [ $(($epoch_rain_start-$spot_timeout)) -lt $epoch_current ] && [ $(($epoch_rain_stop+$spot_timeout)) -gt $epoch_current ]; then
+    echo "It will rain or has been raining within the next $spot_timeout seconds, so turn off the spots"
     switchrelais[${relaisnamepin["Spot Stein"]}]=0
     switchrelais[${relaisnamepin["Spot Ecke"]}]=0
   fi
-  if [ [ $epoch_rain_start -ge $epoch_current ] && [ $epoch_rain_stop -le $epoch_current ] ]; then
+  if [ $epoch_current -ge $epoch_rain_start ] && [ $epoch_current -le $epoch_rain_stop ]; then
     switchrelais[${relaisnamepin["Regenmaschine"]}]=1
   else
     switchrelais[${relaisnamepin["Regenmaschine"]}]=0
@@ -19,7 +19,7 @@ if [ $epoch_rain_start -gt 0 ]; then
   # This trick turns the rainmachine off for one minute every three minutes
   # It is needed becaus the rain machine will stop after about 3 three minutes
   # and only restart 15 minutes later
-  if [ $((($epoch_current/60) % 3)) -ne 0 ] ; then
+  if [ $((($epoch_current/60) % 3)) -eq 0 ] ; then
     switchrelais[${relaisnamepin["Regenmaschine"]}]=0
   fi
 else
