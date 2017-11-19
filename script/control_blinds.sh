48fef79c36d18522f6675ef8a5fd1409ad9fb32a
Eike Holtz <github@eholtz.de>
2017-11-19 15:34:07 +0000

    separate times for down and up

--- script/control_blinds.sh
+++ script/control_blinds.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # move 12 minutes
-movetime=$((12*60))
+movetime_up=$((13*60))
+movetime_down=$((11*60))
 
 # so lange current > start daemmerung und current < ( start daemmerung + movetime )
 
-if [ $epoch_current -ge $epoch_morgendaemmerung_start ] && [ $epoch_current -lt $(($epoch_morgendaemmerung_start+$movetime)) ] ; then
+if [ $epoch_current -ge $epoch_morgendaemmerung_start ] && [ $epoch_current -lt $(($epoch_morgendaemmerung_start+$movetime_up)) ] ; then
   echo "Roll up the blinds"
   gpio write ${motorpins[2]} 1
 else
@@ -14,7 +15,7 @@ fi
 
 # so lange current > ( stop daemmerung - movetime  ) und current < stop daemmerung
 
-if [ $epoch_current -ge $(($epoch_abenddaemmerung_stop-$movetime)) ] && [ $epoch_current -lt $epoch_abenddaemmerung_stop ] ; then
+if [ $epoch_current -ge $(($epoch_abenddaemmerung_stop-$movetime_down)) ] && [ $epoch_current -lt $epoch_abenddaemmerung_stop ] ; then
   echo "Roll down the blinds"
   gpio write ${motorpins[1]} 1
 else
