5466bf91203b9d18b3119d3e628770627b0a58cc
Eike Holtz <eholtz@ehtplin.iso23270.int>
2018-04-25 22:34:55 +0200

    better mean calculation and code cleanup

--- source/sht1x/read_sensor.c
+++ source/sht1x/read_sensor.c
@@ -35,172 +35,109 @@ GPIO Pins can be changed in the Defines of RPi_SHT1x.h
 #include "RPi_SHT1x_##MYPIN##.h"
 #include <math.h>
 
-#define NUMMEASURES 3
+#define NUMMEASURES 11
 
-float calcsd(float data[]);
-float calcmean(float data[]);
+float calcsd(float data[],float min, float max);
+float calcmean(float data[], float min, float max);
 
-float gettemp(void) {
-	value humi_val,temp_val;
+float gettemp(void)
+{
+	value humi_val, temp_val;
 	delay(23);
 	SHT1x_InitPins();
 	SHT1x_Reset();
-	SHT1x_Measure_Start( SHT1xMeaT );
-	SHT1x_Get_Measure_Value( (unsigned short int*) &temp_val.i );
+	SHT1x_Measure_Start(SHT1xMeaT);
+	SHT1x_Get_Measure_Value((unsigned short int *)&temp_val.i);
 	temp_val.f = (float)temp_val.i;
 	humi_val.f = 0.0;
 	SHT1x_Calc(&humi_val.f, &temp_val.f);
 	return temp_val.f;
 }
-	
-float gethumi(void) {
-	value humi_val,temp_val;
+
+float gethumi(void)
+{
+	value humi_val, temp_val;
 	delay(23);
 	SHT1x_InitPins();
 	SHT1x_Reset();
-	SHT1x_Measure_Start( SHT1xMeaRh );
-	SHT1x_Get_Measure_Value( (unsigned short int*) &humi_val.i );
+	SHT1x_Measure_Start(SHT1xMeaRh);
+	SHT1x_Get_Measure_Value((unsigned short int *)&humi_val.i);
 	humi_val.f = (float)humi_val.i;
 	temp_val.f = 0.0;
 	SHT1x_Calc(&humi_val.f, &temp_val.f);
 	return humi_val.f;
 }
 
-
-int printTempAndHumidity(void)
+int th(void)
 {
-  unsigned char noError = 1;  
-value humi_val,temp_val;
-
-  unsigned int getValues = 0;
-  unsigned int x = 0;
-
-  // Try to read 10 times
-  for(x=0;x<10;x++) {
-
-    // Wait at least 11ms after power-up (chapter 3.1)
-    delay(23); 
-
-    // Set up the SHT1x Data and Clock Pins
-    SHT1x_InitPins();
-
-    // Reset the SHT1x
-    SHT1x_Reset();
-
-    // Request Temperature measurement
-    noError = SHT1x_Measure_Start( SHT1xMeaT );
-    if (noError) {
-      getValues++;
-    }
-
-    // Read Temperature measurement
-    noError = SHT1x_Get_Measure_Value( (unsigned short int*) &temp_val.i );
-    if (noError) {
-      getValues++;
-    }
-
-    // Request Humidity Measurement
-    noError = SHT1x_Measure_Start( SHT1xMeaRh );
-    if (noError) {
-      getValues++;
-    }
-
-    // Read Humidity measurement
-    noError = SHT1x_Get_Measure_Value( (unsigned short int*) &humi_val.i );
-    if (noError) {
-      getValues++;
-    }
-
-    if (getValues>=4) {
-      x=20;
-    }
-  }
-
-  if (x<20) {
-    printf("NaN\nNaN\n");
-    return 1;
-  }
-
-  // Convert intergers to float and calculate true values
-  temp_val.f = (float)temp_val.i;
-  humi_val.f = (float)humi_val.i;
-
-  // Calculate Temperature and Humidity
-  SHT1x_Calc(&humi_val.f, &temp_val.f);
-
-  //Print the Temperature to the console
-  //printf("Temperature: %0.2f%cC\n",temp_val.f,0x00B0);
-  printf("%0.2f\n",temp_val.f);
-
-  //Print the Humidity to the console
-  //printf("Humidity: %0.2f%%\n",humi_val.f);
-  printf("%0.2f\n",humi_val.f);
-
-  //Calculate and print the Dew Point
-  //float fDewPoint;
-  //SHT1x_CalcDewpoint(humi_val.f ,temp_val.f, &fDewPoint);
-  //printf("Dewpoint: %0.2f%cC\n",fDewPoint,0x00B0);
-  return 0;
-}
-
-int th(void) {
-	int x=0,y=0;
-	int maxtries=10;
+	int x = 0, y = 0;
+	int maxtries = 10;
 
 	float temp[NUMMEASURES];
 	float humi[NUMMEASURES];
 
-	float sumsd=0.0;
+	float sumsd = 0.0;
 
-	for(x=0;x<maxtries;x++) {
-		for(y=0;y<NUMMEASURES;y++) {
-			temp[y]=gettemp();
-			humi[y]=gethumi();
-			sumsd=calcsd(temp)+calcsd(humi);
-			if (sumsd<1) {
-				x=maxtries;
+		for (y = 0; y < NUMMEASURES; y++)
+		{
+			temp[y] = gettemp();
+			humi[y] = gethumi();
+			sumsd = calcsd(temp,15,50) + calcsd(humi,40,100);
+			if (sumsd < 1)
+			{
+				x = maxtries;
 			}
 			//			printf("std dev temp %f\n",calcsd(temp));
 			//			printf("std dev humi %f\n",calcsd(humi));
 		}
+	if (sumsd < 1)
+	{
+		printf("%0.2f\n%0.2f\n", calcmean(temp,15,50), calcmean(humi,40,100));
+		return 0;
 	}
-	if (sumsd<1) {
-		printf("%0.2f\n%0.2f\n",calcmean(temp),calcmean(humi));
-	} else {
+	else
+	{
 		printf("NaN\nNaN\n");
-	}	
-}
-
-float calcmean(float data[]) {
-	float sum=0.0, mean=0.0;
-	int i=0;
-	for(i=0;i<NUMMEASURES;i++) {
-		sum+=data[i];
-//		printf("data %f\n",data[i]);
+		return 1;
 	}
-	return sum/NUMMEASURES;
 }
 
-float calcsd(float data[]) {
-	float sum=0.0, mean=0.0, sd=0.0;
-	int i=0;
-	mean=calcmean(data);
-//	printf("sum %f\n",sum);
-//	printf("mean %f\n",mean);
-	for(i=0;i<NUMMEASURES;i++) {
-		sd+=pow(data[i]-mean,2);
+float calcmean(float data[], float min, float max)
+{
+	float sum = 0.0, mean = 0.0;
+	int i = 0;
+	for (i = 0; i < NUMMEASURES; i++)
+	{
+		if ((data[i]>min) && (data[i]>max)) {
+			sum += data[i];
+		}
+		//		printf("data %f\n",data[i]);
 	}
-	return sqrt(sd/NUMMEASURES);
+	return sum / NUMMEASURES;
 }
 
+float calcsd(float data[], float min, float max)
+{
+	float sum = 0.0, mean = 0.0, sd = 0.0;
+	int i = 0;
+	mean = calcmean(data,min,max);
+	//	printf("sum %f\n",sum);
+	//	printf("mean %f\n",mean);
+	for (i = 0; i < NUMMEASURES; i++)
+	{
+		if ((data[i]<max) && (data[i]>min)) {
+			sd += pow(data[i] - mean, 2);
+		}
+	}
+	return sqrt(sd / NUMMEASURES);
+}
 
-int main ()
+int main()
 {
 	//Initialise the Raspberry Pi GPIO
-	if(!bcm2835_init())
+	if (!bcm2835_init())
 		return 1;
 
 	return th();
 	//  return printTempAndHumidity();
-
 }
