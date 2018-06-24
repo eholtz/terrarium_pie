aa57372c5df0928dc193c69cbd63c70c5d6d6bd5
Eike Holtz <github@eholtz.de>
2018-06-24 12:24:14 +0200

    years for the raindaemon

--- source/raindaemon.cpp
+++ source/raindaemon.cpp
@@ -20,20 +20,38 @@ void writepin(int state) {
   }
 }
 
-int main() {
+void createraindays() {
+  // we use the current year as seed
   time_t now = time(0);
   struct tm *nowt = localtime(&now);
-  unsigned short state = 0, laststate = 0;
-  ofstream outhandler;
-  ifstream inhandler;
-  char buffer[128];
-  unsigned short rainprobability = 256 / 4;
-  unsigned long rainduration = 0;
-  unsigned int rainstartsecond = 0, currentsecond = 0;
-  string line;
 
-  // startup
-  cout << "starting up ..." << endl;
+  // we will have to do every day of the year
+  // and we'll let c++ do the calculation for us
+  time_t calctime = time(0);
+  struct tm *calctimet;
+
+  // buffer for filename and the actual
+  // file handler
+  char buffer[128];
+  ofstream outhandler;
+
+  // definition of rain times.
+  // rainpause = 4 => it will rain about every 4 days
+  // raindurationmin/raindurationmax is in seconds
+  // rainstarthour/rainstophour should be configured in
+  // a way it does not interfere with the noon light
+  static unsigned short RAINPROBMAX = 256;
+  static unsigned short RAINPAUSE = 4;
+  static unsigned short DURATIONMIN = 60;
+  static unsigned short DURATIONMAX = 240;
+  static unsigned short RAINSTARTOUR = 13;
+  static unsigned short RAINSTOPHOUR = 20;
+
+  unsigned short rainprobability = RAINPROBMAX / RAINPAUSE;
+  unsigned long rainduration = 0;
+  unsigned int rainstartsecond = 0;
+  unsigned short state = 0;
+
   cout << "creating rain directory" << endl;
   // mkdir did not work for some strange reason, so I'm lazy
   snprintf(buffer, sizeof(buffer), "mkdir -p /dev/shm/rain/");
@@ -43,23 +61,29 @@ int main() {
   // states across reboots
   cout << "calculating rain days ... " << endl;
   srand(nowt->tm_year);
-  for (int i = 0; i < 365; i++) {
+  calctimet = localtime(&calctime);
+  calctimet->tm_yday = 0;
+  calctimet->tm_hour = 1;
+  calctimet->tm_min = 0;
+  calctimet->tm_sec = 0;
+  calctime = mktime(calctimet);
+  while (calctimet->tm_year == nowt->tm_year) {
     // cout << i << endl;
-    if ((rand() % 255) < rainprobability) {
+    if ((rand() % RAINPROBMAX) < rainprobability) {
       state = 1;
-      rainduration = rand() % 60 * 3 + 60;
-      // we will rain between 13:00 and 18:00
-      // so we will never start when the noon light is on
-      // heating should recognize rain events and act
-      // accordingly
-      rainstartsecond = (13 + rand() % 4) * 3600 + rand() % 60 * 60 + rand() % 60;
+      rainduration = rand() % (DURATIONMAX - DURATIONMIN) + DURATIONMIN;
+      // because we will add a random hour to the calculated value
+      // we have to subtract one from the difference of rainstop and
+      // rainstart
+      rainstartsecond =
+          (RAINSTARTOUR + rand() % (RAINSTOPHOUR - RAINSTARTOUR - 1)) * 3600 + rand() % 60 * 60 + rand() % 60;
       rainprobability = 0;
     } else {
       rainduration = 0;
       rainstartsecond = 0;
-      rainprobability += 256 / 4;
+      rainprobability += RAINPROBMAX / RAINPAUSE;
     }
-    snprintf(buffer, sizeof(buffer), "/dev/shm/rain/%d", i);
+    snprintf(buffer, sizeof(buffer), "/dev/shm/rain/%d", calctimet->tm_yday);
     outhandler.open(buffer);
     if (outhandler.is_open()) {
       outhandler << rainduration << endl;
@@ -68,8 +92,32 @@ int main() {
     } else {
       cout << "ERROR: could not write to " << buffer << endl;
     }
+    // we add two seconds more to each day to
+    // compensate for leap seconds. this may be a bit overkill
+    // but, hey ...
+    // it will not be a problem in the long run, because we only
+    // calculate times for one year and we only have 366*2 seconds
+    // (leap year) which is less than a day.
+    calctime += 86402;
+    calctimet = localtime(&calctime);
   }
   cout << "calculating done." << endl;
+}
+
+int main() {
+  time_t now = time(0);
+  struct tm *nowt = localtime(&now);
+  time_t start = time(0);
+  struct tm *startt = localtime(&start);
+  unsigned long rainduration = 0;
+  unsigned int rainstartsecond = 0, currentsecond = 0;
+  unsigned short state = 0, laststate = 0;
+  ifstream inhandler;
+  char buffer[128];
+  string line;
+
+  // startup
+  cout << "starting up ..." << endl;
 
   // init the file with 0
   writepin(0);
@@ -78,6 +126,10 @@ int main() {
   while (true) {
     now = time(0);
     nowt = localtime(&now);
+    // compensate for a new year
+    if (startt->tm_year!=nowt->tm_year) {
+      createraindays();
+    }
     currentsecond = nowt->tm_hour * 3600 + nowt->tm_min * 60 + nowt->tm_sec;
     snprintf(buffer, sizeof(buffer), "/dev/shm/rain/%d", nowt->tm_yday);
     inhandler.open(buffer);
@@ -111,6 +163,8 @@ int main() {
       }
     } else {
       cout << "ERROR: could not read from " << buffer << endl;
+      cout << "trying to recalculate rain days" << endl;
+      createraindays();
     }
     // sleep for 10 seconds
     usleep(10000000);
