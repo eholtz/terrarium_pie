b75981ae1285f19473ad9e83a033447dd814c366
Eike Holtz <github@eholtz.de>
2018-06-24 18:31:31 +0200

    nicer formatting for graphs

--- setup/index.php
+++ setup/index.php
@@ -4,6 +4,17 @@
 <meta charset="utf-8">
 <title>terrarium 1</title>
 <meta http-equiv="refresh" content="60">
+<style>
+li { 
+    float: left;
+    list-style: none; 
+    text-align: center; 
+    border: 1px dashed grey; 
+    width: 5em;
+    margin-left: 1em;
+    margin-bottom: 2em;
+}
+</style>
 </head>
 <body>
 <h1>Aktuelles Bild</h1>
@@ -21,14 +32,51 @@ echo '<a href="' . "./terracam/" . $dayfolders[0] . "/" . $pictures[0] . '"><img
 $ppath = "./rrd/graphs/";
 foreach (scandir($ppath) as $pic) {
     if (substr($pic, -4) == ".png") {
-        $pictures[] = $pic;
+        $data = explode("_", $pic);
+        $span = $data[1];
+        $sort = $data[0];
+        $spans[$sort] = $span;
+        $pictures[$span][] = $pic;
     }
 }
-foreach ($pictures as $pic) {
-    echo "<img src=\"" . $ppath . "/" . $pic . "\">";
+ksort($spans);
+$divs = "";
+foreach ($spans as $span) {
+    sort($pictures[$span]);
+    $divs .= "\n<div style=\"clear:both;display:none;\" id=\"$span\">";
+    foreach ($pictures[$span] as $pic) {
+        $divs .= "<img src=\"" . $ppath . "/" . $pic . "\">";
+    }
+    $divs .= "</div>";
 }
 ?>
-<h1>Alte Bilder</h1>
+<ul>
+<?php
+foreach ($spans as $span) {
+    echo "<li>$span</li>";
+}
+echo $divs;
+?>
+</ul>
+<script>
+function toggle(elementid) {
+    var el=document.getElementById(elementid);
+    if (el) {
+    if (el.style.display == "none") {
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }
+}
+}
+var lis=document.getElementsByTagName("li");
+for (var i=0;i<lis.length;i++) {
+    lis[i].addEventListener("click",function() {
+        toggle(this.innerHTML);
+    });
+}
+</script>
+<h1 style="clear:both;">Alte Bilder</h1>
 <?php
 foreach ($dayfolders as $day) {
     if (strlen($day) != 10) {continue;}
