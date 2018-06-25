9117bbffa8d2f36e7af0ff736e2c403762d23eed
Eike Holtz <github@eholtz.de>
2018-06-25 21:57:48 +0200

    add a stroke to the text

--- cron/terracam.sh
+++ cron/terracam.sh
@@ -19,7 +19,8 @@ if [ $? -eq 0 ]; then
   TZ="Europe/Berlin"
   export TZ
   convert $tmpdir/camtmp.jpg -resize 1920x1080 $tmpdir/camtmp.bmp
-  convert $tmpdir/camtmp.bmp -fill white -font DejaVu-Sans-Bold -pointsize 24 -gravity south -annotate +0+0 "Geckocam $(date +"%F %R %Z")" -quality 80 $tmpdir/camtmptxt.jpg
+  convert $tmpdir/camtmp.bmp -fill white -font DejaVu-Sans-Bold -pointsize 24 -strokewidth 1 -stroke black -gravity south -annotate +0+0 "Geckocam $(date +"%F %R %Z")" -quality 80 $tmpdir/camtmptxt.jpg
+  
   TZ=$ctz
   export TZ
 
